392a4ad33d3a1ff0ab8d144a50a6aa025e7b51cc
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2022-11-22 19:46:10 +0700

    fix router navigation roles'

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -70,212 +70,255 @@
         <!-- Navigation -->
         <ul class="md:flex-col md:min-w-full flex flex-col list-none">
           <li class="items-center pb-2">
-            <router-link :to="{ path: '/Dashboard' }"> <a
-              class="text-primary hover:text-pink-600 text-xs uppercase py-3 font-bold block"
-              ><i class="fas fa-tv opacity-75 mr-2 text-sm"></i> Dashboard</a
-            ></router-link>
-           
+            <router-link :to="{ path: '/Dashboard' }">
+              <a
+                class="text-primary hover:text-pink-600 text-xs uppercase py-3 font-bold block"
+                ><i class="fas fa-tv opacity-75 mr-2 text-sm"></i>Dashboard</a
+              ></router-link
+            >
           </li>
           <li class="inline-flex">
-            <router-link :to="{ path: '/history-checkup' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-table mr-2 text-blueGray-400 text-base"
-              ></i
-              >Riwayat Pemeriksaan</a
-            ></router-link>
+            <router-link :to="{ path: '/history-checkup' }">
+              <a
+                class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase block mb-4 no-underline font-bold"
+                href="#"
+                ><i class="fas fa-table mr-2 text-blueGray-400 text-base"></i
+                >Riwayat Pemeriksaan</a
+              ></router-link
+            >
           </li>
-        </ul>
-        
-        <!-- Divider -->
-        <hr class="my-4 md:min-w-full" />
-        <!-- Heading -->
-        <h6
-          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
-        >
-          Administrasi pages
-        </h6>
-        <!-- Navigation -->
-        <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-          <li class="inline-flex">
-            <router-link :to="{ path: '/administrations/patient-directory' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Data Pasien</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/administrations/patient-registration' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Registrasi Pasien</a
-            ></router-link>
-          </li>
-
         </ul>
 
-        <h6
-          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
-        >
-          Poli pages
-        </h6>
-        <!-- Navigation -->
-        <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-          <li class="inline-flex">
-            <router-link :to="{ path: '/poli/ordering-lab' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Pemesanan Lab</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/poli/report-checkup' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Laporan Pemeriksaan</a
-            ></router-link>
-          </li>
-        </ul>
-        <h6
-          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
-        >
-          Laborat pages
-        </h6>
-        <!-- Navigation -->
-        <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-          <li class="inline-flex">
-            <router-link :to="{ path: '/laborat/queue-lab' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Antrean Pemeriksaan</a
-            ></router-link>
-          </li>
-        </ul>
-        <h6
-          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
-        >
-          Super Admin pages
-        </h6>
-        <!-- Navigation -->
-        <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/create-user' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Tambah Petugas</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/create-sector' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Tambah Bidang</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/create-parameter' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Tambah Parameter</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/create-methode' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Tambah Metode</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/create-validator' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Tambah Validator</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/employee-directory' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Data Petugas</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/sector-directory' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Data Bidang</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/parameter-directory' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Data Parameter</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/methode-directory' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Data Metode</a
-            ></router-link>
-          </li>
-          <li class="inline-flex">
-            <router-link :to="{ path: '/superadmin/validator-directory' }">            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-              href="#"
-              ><i
-                class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
-              ></i
-              >Data Validator</a
-            ></router-link>
-          </li>
-        </ul>
-        
+          <!-- Divider -->
+          <hr class="my-4 md:min-w-full" />
+
+        <div v-if="roles == 1 || roles == 2">
+
+          <!-- Heading -->
+          <h6
+            class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          >
+            Administrasi pages
+          </h6>
+          <!-- Navigation -->
+          <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
+            <li class="inline-flex">
+              <router-link :to="{ path: '/administrations/patient-directory' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Data Pasien</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link
+                :to="{ path: '/administrations/patient-registration' }"
+              >
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Registrasi Pasien</a
+                ></router-link
+              >
+            </li>
+          </ul>
+        </div>
+
+        <div v-if="roles == 1 || roles == 4">
+          <h6
+            class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          >
+            Poli pages
+          </h6>
+          <!-- Navigation -->
+          <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
+            <li class="inline-flex">
+              <router-link :to="{ path: '/poli/ordering-lab' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Pemesanan Lab</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/poli/report-checkup' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Laporan Pemeriksaan</a
+                ></router-link
+              >
+            </li>
+          </ul>
+        </div>
+
+        <div v-if="roles == 1 || roles == 3">
+          <h6
+            class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          >
+            Laborat pages
+          </h6>
+          <!-- Navigation -->
+          <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
+            <li class="inline-flex">
+              <router-link :to="{ path: '/laborat/queue-lab' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Antrean Pemeriksaan</a
+                ></router-link
+              >
+            </li>
+          </ul>
+        </div>
+
+        <div v-if="roles == 1">
+          <h6
+            class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
+          >
+            Super Admin pages
+          </h6>
+          <!-- Navigation -->
+          <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/create-user' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Tambah Petugas</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/create-sector' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Tambah Bidang</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/create-parameter' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Tambah Parameter</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/create-methode' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Tambah Metode</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/create-validator' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Tambah Validator</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/employee-directory' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Data Petugas</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/sector-directory' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Data Bidang</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/parameter-directory' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Data Parameter</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/methode-directory' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Data Metode</a
+                ></router-link
+              >
+            </li>
+            <li class="inline-flex">
+              <router-link :to="{ path: '/superadmin/validator-directory' }">
+                <a
+                  class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
+                  href="#"
+                  ><i
+                    class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"
+                  ></i
+                  >Data Validator</a
+                ></router-link
+              >
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
@@ -287,6 +330,7 @@ export default {
   data() {
     return {
       collapseShow: "hidden",
+      roles: sessionStorage.getItem("roles_id"),
     };
   },
   methods: {
