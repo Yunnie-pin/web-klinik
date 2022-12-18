dde9c89eed7139b5811f753ea3e0061376af4195
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2022-12-18 13:53:25 +0700

    apaya

--- frontend/src/views/HistoryCheckUp.vue
+++ frontend/src/views/HistoryCheckUp.vue
@@ -158,14 +158,14 @@
                             <li>
                               <a
                                 class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-                                :href="API_URL + 'api/cetak/hasil/'+pemeriksaan.status.id" target="_blank"
+                                :href="API_URL + 'api/cetak/hasil/'+pemeriksaan.id" target="_blank"
                                 >Cetak Hasil</a
                               >
                             </li>
                             <li>
                               <a
                                 class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-                                :href="API_URL + 'api/cetak/nota/'+pemeriksaan.status.id" target="_blank"
+                                :href="API_URL + 'api/cetak/nota/'+pemeriksaan.id" target="_blank"
                                 >Cetak Nota</a
                               >
                             </li>
